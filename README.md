942e17bf3e6aa799646de74249a9ecc0d3d451ef
Filip Krunic <fkrunic9@gmail.com>
2023-09-03 12:19:07 -0700

    project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Stats Preview Card Component
+Implementation of [Stats Preview Card Component](https://www.frontendmentor.io/challenges/stats-preview-card-component-8JqbgoU62) using Vue and TailwindCSS.
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+</style>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      bodyClass: ''
+    };
+  },
+  mounted() {
+    document.body.className = this.bodyClass;
+  },
+  beforeDestroy() {
+    // Reset the class when the component is destroyed
+    document.body.className = '';
+  }
+};
+</script>
